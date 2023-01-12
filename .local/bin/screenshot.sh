af1452615b9fa7acb1be6ba4ecd4937636f355b2
theNEOkun <sven@raswill.se>
2023-01-12 16:39:31 +0100

    NVim updated

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 sel=$(slop -f "-i %i -g %g")
-shotgun $sel ~/Pictures/screenshots/$(date +%Y-%m-%d-h%Im%Ms%S).png
+shotgun $sel ~/Pictures/screenshots/$(date +%Y-%m-%d-h%Hm%Ms%S).png
